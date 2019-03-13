0b05c6086efd1e184f7ed9721a11060bb6956d9c
Jared Smith <j.danielsmith451@gmail.com>
2019-03-13 13:20:56 -0700

    Submission for second checkpoint

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -16,4 +16,10 @@ describe("MenuController", () => {
       expect(this.menu.getContactCount()).toBe(1)
     });
   });
+
+  describe("remindMe()", () => {
+    it("Phrase should be: Learning is a life-long pursuit", () => {
+      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit")
+    });
+  })
 });
